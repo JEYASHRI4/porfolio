40c8113b9b709b8f6592651c6ece6495ff87f97e
JEYASHRI4 <jeyashri467@gmail.com>
2023-04-05 14:50:42 +0530

    portfolio

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import photo from './photo.png';
+export default function About() {
+  return (
+    <div className='about'>
+    <img src={photo}></img>
+  <h1 >HAI I AM JEYA SHRI</h1>  <p id='about'>I am currently pursuring my <b>BE.EEE</b> in SRI KRISHNA COLLEGE OF ENGINEERING AND TECHNOLOGY.<br></br>
+  My native is Sivakasi known for its production of crackers .I love to visit new places and to experience
+  new customs and traditions .<br></br>
+  I love to code .I am good at c++,html and css
+  </p>
+    </div>
+  )
+}
---
+++ src/components/contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Contact() {
+  const handlesubmit = () =>{
+    alert('Thanks For contacting myself');
+  }
+  return (
+
+    <div>
+    <form className='form'><h1>TO CONTACT ME</h1>
+    <h2>ENTER YOUR NAME:</h2>
+    <input type="text" maxLength={40}
+    size="40" placeholder='ENTER YOUR NAME'></input>
+    <br></br>
+    <h2>ENTER YOUR EMAIL-ID :</h2>
+    <input type="email" size="40"></input>
+    <br></br>
+    <br></br>
+    <button type='button' onClick={()=>handlesubmit()}>SUBMIT</button>
+    </form>
+    </div>
+  )
+}
